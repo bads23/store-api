307f36df61d178ee38ad32982ec470d422ae2bbe
Steve <stevekaruma@gmail.com>
2019-04-05 21:18:28 +0300

    added django-heroku

--- store_api/settings.py
+++ store_api/settings.py
@@ -150,16 +150,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = '/opt/store/api/media/'
 MEDIA_URL = '/media/'
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)

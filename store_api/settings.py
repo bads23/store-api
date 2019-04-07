01cfe2f7515608c0104c7c871b100558a4e848e8
Steve <stevekaruma@gmail.com>
2019-04-07 23:27:07 +0300

    db settings

--- store_api/settings.py
+++ store_api/settings.py
@@ -100,18 +100,6 @@ DATABASES = {
     )
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'store_api',
-#         'USER': 'badteam',
-#         'PASSWORD': 'badteam',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 

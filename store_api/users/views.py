cb5385d9d7d3a38d6b9ad2e45c7b839022673bb4
Steve <stevekaruma@gmail.com>
2019-04-20 08:47:15 +0300

    Add me view for logged in user

--- store_api/users/views.py
+++ store_api/users/views.py
@@ -1,6 +1,7 @@
 from . import models
 from . import serializer
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
 
@@ -18,6 +19,11 @@ class CustomUserViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(detail=False)
+    def me(self, request):
+        serialized = serializer.CustomUserSerializer(request.user)
+        return Response(serialized.data)
 
 
 class UserDetailsViewSet(viewsets.ModelViewSet):

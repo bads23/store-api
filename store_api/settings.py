661d28cbe25b6084bcb07e915eb22740054c50a4
Steve <stevekaruma@gmail.com>
2019-05-03 22:23:52 +0300

    change static folder

--- store_api/settings.py
+++ store_api/settings.py
@@ -159,7 +159,7 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (

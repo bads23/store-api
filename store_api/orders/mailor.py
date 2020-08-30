ac65fe1abdfaa9adacf4bcd88bff9fc2acdc3c46
Steve Karuma <stevekaruma@gmail.com>
2020-08-30 22:54:42 +0300

    update reset password url

--- store_api/orders/mailor.py
+++ store_api/orders/mailor.py
@@ -85,7 +85,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     context = {
         'first_name': reset_password_token.user.first_name,
         'email': reset_password_token.user.email,
-        'reset_password_url': "{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
+        'reset_password_url': "https://store.motiontalentafrica.co.ke/?token={}".format(reset_password_token.key)
     }
 
     # render email text
